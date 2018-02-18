07280fb3322f6ac3ea9d21d610511eb2973ca896
KingMus <marcofm47@gmail.com>
2018-02-18 21:33:42 +0100

    added pragma reduction to pi.c

--- pi/pi.c
+++ pi/pi.c
@@ -6,25 +6,26 @@
 #define TRYS 5000000
 
 static int throw() {
-  double x, y;
-  x = (double)rand() / (double)RAND_MAX;
-  y = (double)rand() / (double)RAND_MAX;
-  if ((x*x + y*y) <= 1.0) return 1;
-    
-  return 0;
+	double x, y;
+	x = (double) rand() / (double) RAND_MAX;
+	y = (double) rand() / (double) RAND_MAX;
+	if ((x * x + y * y) <= 1.0)
+		return 1;
+
+	return 0;
 }
 
 int main(int argc, char **argv) {
-  int globalCount = 0, globalSamples=TRYS;
+	int globalCount = 0, globalSamples = TRYS;
 
-
-  for(int i = 0; i < globalSamples; ++i) {
+#pragma omp parallel for reduction ( +:globalCount)  num_threads(4)
+	for (int i = 0; i < globalSamples; ++i) {
 		globalCount += throw();
-  }
+	}
 
-  double pi = 4.0 * (double)globalCount / (double)(globalSamples);
- 
-  printf("pi is %.9lf\n", pi);
-  
-  return 0;
+	double pi = 4.0 * (double) globalCount / (double) (globalSamples);
+
+	printf("pi is %.9lf\n", pi);
+
+	return 0;
 }
