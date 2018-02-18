301a1d235a5e445aca808b7b75dfa56d4a2df5c3
KingMus <marcofm47@gmail.com>
2018-02-18 01:00:16 +0100

    updated code with pragma parallel section

--- hello-world/hello-world.c
+++ hello-world/hello-world.c
@@ -5,11 +5,31 @@
 
 int main(int argc, char **argv) {
 
-#pragma omp parallel
+#pragma omp parallel sections num_threads(4)
+{
+
+	#pragma omp section
 	{
-		printf("Hello World from %d thread of %d \n", omp_get_thread_num(),
-				omp_get_max_threads());
+	printf("Hallo Welt from %d thread of %d \n", omp_get_thread_num(),
+			omp_get_max_threads());
+	printf("Hej varlden from %d thread of %d \n", omp_get_thread_num(),
+			omp_get_max_threads());
 	}
+	#pragma omp section
+	{
+	printf("Bonjour tout le monde from %d thread of %d \n",
+			omp_get_thread_num(), omp_get_max_threads());
+	}
+	#pragma omp section
+	{
+		printf("Hola mundo from %d thread of %d \n", omp_get_thread_num(),	omp_get_max_threads());
+	}
+	#pragma omp section
+	{
+	printf("Hello World from %d thread of %d \n", omp_get_thread_num(),
+			omp_get_max_threads());
+	}
+}
 
   return 0;
 }
