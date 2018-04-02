8a0f155ba3d5ac1c60ec646f989ce65022e7f661
KingMus <bomberurs+it@gmail.com>
2018-04-02 20:33:03 +0200

    changed vtk output

--- gameoflife_mpi/gameoflife.c
+++ gameoflife_mpi/gameoflife.c
@@ -20,8 +20,8 @@ long TimeSteps = 100;
  * Sonst sind die Randfelder doppelt vorhanden und jede Datei ist größer als sie sein muss.
  */
 
-void writeVTK2(long timestep, double *data, char prefix[1024], int wstart,
-		int hstart, long w, long h, char threadnum[1024]) {
+void writeVTK2(long timestep, double *data, char prefix[1024], long w, long h,
+		char threadnum[1024]) {
 	char filename[2048];
 	int x, y;
 
@@ -29,7 +29,7 @@ void writeVTK2(long timestep, double *data, char prefix[1024], int wstart,
 	long offsetY = 0;
 	float deltax = 1.0;
 	float deltay = 1.0;
-	long nxy = (w - wstart) * (h - hstart) * sizeof(float);
+	long nxy = w * h * sizeof(float);
 
 	mkdir("vtk_folder", 0777);
 
@@ -43,8 +43,8 @@ void writeVTK2(long timestep, double *data, char prefix[1024], int wstart,
 			"<VTKFile type=\"ImageData\" version=\"0.1\" byte_order=\"LittleEndian\" header_type=\"UInt64\">\n");
 	fprintf(fp,
 			"<ImageData WholeExtent=\"%d %d %d %d %d %d\" Origin=\"0 0 0\" Spacing=\"%le %le %le\">\n",
-			offsetX, offsetX + (w - wstart), offsetY, offsetY + (h - hstart), 0,
-			0, deltax, deltax, 0.0);
+			offsetX, offsetX + w, offsetY, offsetY + h, 0, 0, deltax, deltax,
+			0.0);
 	fprintf(fp, "<CellData Scalars=\"%s\">\n", prefix);
 	fprintf(fp,
 			"<DataArray type=\"Float32\" Name=\"%s\" format=\"appended\" offset=\"0\"/>\n",
@@ -55,12 +55,10 @@ void writeVTK2(long timestep, double *data, char prefix[1024], int wstart,
 	fprintf(fp, "_");
 	fwrite((unsigned char*) &nxy, sizeof(long), 1, fp);
 
-	for (x = wstart; x < w; x++) {
-		for (y = hstart; y < h; y++) {
+	for (x = 0; x < w; x++) {
+		for (y = 0; y < h; y++) {
 
-			int correctIndex = getCorrectIndex(w, h, threadnum);
-
-			float value = data[calcIndex(correctIndex, y, x)];
+			float value = data[calcIndex(h, y, x)];
 
 			fwrite((unsigned char*) &value, sizeof(float), 1, fp);
 		}
@@ -69,17 +67,6 @@ void writeVTK2(long timestep, double *data, char prefix[1024], int wstart,
 	fprintf(fp, "\n</AppendedData>\n");
 	fprintf(fp, "</VTKFile>\n");
 	fclose(fp);
-}
-
-int getCorrectIndex(long w, long h, char threadnum[1024]) {
-	long correctIndex = w;
-	if (h > correctIndex) {
-		correctIndex = h;
-	}
-	if (threadnum == "_r0_") {
-		correctIndex = h * 2;
-	}
-	return correctIndex;
 }
 
 void show(double* currentfield, int w, int h) {
@@ -123,11 +110,12 @@ void game(int w, int h) {
 
 	filling(currentfield, w, h);
 
-	long t;
 	int rank, size;
 
-	MPI_Status statusRight;
-	MPI_Status statusLeft;
+	MPI_Status statusOben;
+	MPI_Status statusUnten;
+	MPI_Status statusRechts;
+	MPI_Status statusLinks;
 
 	MPI_Comm comm_cart;
 
@@ -143,136 +131,194 @@ void game(int w, int h) {
 	MPI_Cart_create(MPI_COMM_WORLD, 1, numProcessPerDimension, periodic,
 			reorder, &comm_cart);
 
-	int sendenRechtsGhost[h / 2];
-	int sendenLinksGhost[h / 2];
+	int *part_field_with_ghost = calloc(((w / 2) + 2) * ((h / 2) + 2),
+			sizeof(double));
+	int *part_field = calloc((w / 2) * (h / 2), sizeof(double));
+
+	/*Setze Start- und End-Values
+	 * ------------------------------------------------------------------------------------------------------------------------------------
+	 */
+
+	int xstart, ystart, xende, yende;
 
 	if (rank == 0) {
-
-		int xstart, ystart, xende, yende;
-
 		xstart = 0;
 		xende = w / 2;
 		ystart = 0;
 		yende = h / 2;
+	} else if (rank == 1) {
 
+		xstart = 0;
+		xende = w / 2;
+		ystart = h / 2;
+		yende = h;
+	} else if (rank == 2) {
 
-		printf("--------------------------------------------\n");
+		xstart = w / 2;
+		xende = w;
+		ystart = 0;
+		yende = h / 2;
+	} else if (rank == 3) {
+		xstart = w / 2;
+		xende = w;
+		ystart = h / 2;
+		yende = h;
+	}
 
-		printf("My rank: %d\n", rank);
+	/*Für jeden Prozess wird das passende Teilfeld aus dem großen currentfield geschrieben (ohne Ghostrand-Befüllung)
+	 * ------------------------------------------------------------------------------------------------------------------------------------
+	 */
 
-		printf("Section: von P(%d|%d) bis P(%d|%d) \n\n", xstart,ystart,xende,yende);
-		printf("Position:\n");
-		printf("X0\n");
-		printf("00\n");
+	for (int x = 0; x < w / 2; x++) {
+		for (int y = 0; y < h / 2; y++) {
+			part_field_with_ghost[calcIndex(w, x + 1, y + 1)] =
+					currentfield[calcIndex(w, x + xstart, y + ystart)];
+		}
+	}
 
-		int *rankleft;
-		int *rankright;
+	/*Für den Randaustausch benötigte Variablen.
+	 * ------------------------------------------------------------------------------------------------------------------------------------
+	 */
 
-		MPI_Cart_shift(comm_cart, 1, 1, &rankleft, &rankright);
+	int linksGhost[h / 2];
+	int rechtsGhost[h / 2];
+	int obenGhost[w / 2];
+	int untenGhost[w / 2];
 
-		printf("Neighbour-ranks: %d (left) - %d (right) \n\n", rankleft,
-				rankright);
+	int sendenRechtsGhost[h / 2];
+	int sendenLinksGhost[h / 2];
+	int sendenObenGhost[w / 2];
+	int sendenUntenGhost[w / 2];
 
-		writeVTK2(TimeSteps, currentfield, "gol", xstart, ystart, xende, yende,
-				"_r0_");
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	/*Der von diesem Prozess zu sendende Ghostrand wird aus dem prozeseigenen part_field geschrieben.
+	 * ------------------------------------------------------------------------------------------------------------------------------------
+	 */
+
+	for (int y = 1; y < (h / 2); y++) {
+		sendenLinksGhost[y - 1] = part_field_with_ghost[calcIndex(w, 1, y)];
+		sendenRechtsGhost[y - 1] = part_field_with_ghost[calcIndex(w, (h / 2),
+				y)];
+	}
+
+	for (int x = 1; x < (w / 2); x++) {
+		sendenObenGhost[x - 1] = part_field_with_ghost[calcIndex(w, x, 1)];
+		sendenUntenGhost[x - 1] =
+				part_field_with_ghost[calcIndex(w, x, (h / 2))];
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	/* Jeder Prozess sendet jetzt seinen gerade erstellen Ghostrand seines part_fieldes.
+	 * Außerdem recieved jeder Prozess den zu ihm passenden Ghostrand und speichert ihn in ...
+	 * ------------------------------------------------------------------------------------------------------------------------------------
+	 */
+
+	if (rank == 0) {
+
+		outputProcessInformation(rank, xstart, ystart, xende, yende, comm_cart);
+
+		MPI_Recv(&obenGhost, 14, MPI_INT, 2, 96, MPI_COMM_WORLD, &statusOben);
+		MPI_Recv(&rechtsGhost, 14, MPI_INT, 1, 97, MPI_COMM_WORLD,
+				&statusRechts);
+		MPI_Recv(&untenGhost, 14, MPI_INT, 2, 98, MPI_COMM_WORLD, &statusUnten);
+		MPI_Recv(&linksGhost, 14, MPI_INT, 1, 99, MPI_COMM_WORLD, &statusLinks);
+
+		MPI_Send(&sendenUntenGhost, 14, MPI_INT, 2, 96, MPI_COMM_WORLD);
+		MPI_Send(&sendenRechtsGhost, 14, MPI_INT, 1, 97, MPI_COMM_WORLD);
+		MPI_Send(&sendenObenGhost, 14, MPI_INT, 2, 98, MPI_COMM_WORLD);
+		MPI_Send(&sendenLinksGhost, 14, MPI_INT, 1, 99, MPI_COMM_WORLD);
 
 	}
 	if (rank == 1) {
 
-		int xstart, ystart, xende, yende;
+		outputProcessInformation(rank, xstart, ystart, xende, yende, comm_cart);
 
-		xstart = 0;
-		xende = w / 2;
-		ystart = h / 2;
-		yende = h;
+		MPI_Send(&sendenUntenGhost, 14, MPI_INT, 3, 96, MPI_COMM_WORLD);
+		MPI_Send(&sendenRechtsGhost, 14, MPI_INT, 0, 97, MPI_COMM_WORLD);
+		MPI_Send(&sendenObenGhost, 14, MPI_INT, 3, 98, MPI_COMM_WORLD);
+		MPI_Send(&sendenLinksGhost, 14, MPI_INT, 0, 99, MPI_COMM_WORLD);
 
-
-		printf("--------------------------------------------\n");
-
-		printf("My rank: %d\n", rank);
-
-		printf("Section: von P(%d|%d) bis P(%d|%d) \n\n", xstart,ystart,xende,yende);
-		printf("Position:\n");
-		printf("00\n");
-		printf("X0\n");
-
-		int * rankleft;
-		int *rankright;
-
-		MPI_Cart_shift(comm_cart, 1, 1, &rankleft, &rankright);
-
-		printf("Neighbour-ranks: %d (left) - %d (right) \n\n", rankleft,
-				rankright);
-
-		writeVTK2(TimeSteps, currentfield, "gol", xstart, ystart, xende, yende,
-				"_r1_");
-
+		MPI_Recv(&obenGhost, 14, MPI_INT, 3, 96, MPI_COMM_WORLD, &statusOben);
+		MPI_Recv(&rechtsGhost, 14, MPI_INT, 0, 97, MPI_COMM_WORLD,
+				&statusRechts);
+		MPI_Recv(&untenGhost, 14, MPI_INT, 3, 98, MPI_COMM_WORLD, &statusUnten);
+		MPI_Recv(&linksGhost, 14, MPI_INT, 0, 99, MPI_COMM_WORLD, &statusLinks);
 
 	}
 	if (rank == 2) {
 
-		int xstart, ystart, xende, yende;
+		outputProcessInformation(rank, xstart, ystart, xende, yende, comm_cart);
 
-		xstart = w / 2;
-		xende = w;
-		ystart = 0;
-		yende = h / 2;
+		MPI_Send(&sendenUntenGhost, 14, MPI_INT, 0, 96, MPI_COMM_WORLD);
+		MPI_Send(&sendenRechtsGhost, 14, MPI_INT, 3, 97, MPI_COMM_WORLD);
+		MPI_Send(&sendenObenGhost, 14, MPI_INT, 0, 98, MPI_COMM_WORLD);
+		MPI_Send(&sendenLinksGhost, 14, MPI_INT, 3, 99, MPI_COMM_WORLD);
 
-
-		printf("--------------------------------------------\n");
-
-		printf("My rank: %d\n", rank);
-
-		printf("Section: von P(%d|%d) bis P(%d|%d) \n\n", xstart,ystart,xende,yende);
-		printf("Position:\n");
-		printf("0X\n");
-		printf("00\n");
-
-		int * rankleft;
-		int *rankright;
-
-		MPI_Cart_shift(comm_cart, 1, 1, &rankleft, &rankright);
-
-		printf("Neighbour-ranks: %d (left) - %d (right) \n\n", rankleft,
-				rankright);
-
-		writeVTK2(TimeSteps, currentfield, "gol", xstart, ystart, xende, yende,
-				"_r2_");
-
+		MPI_Recv(&obenGhost, 14, MPI_INT, 0, 96, MPI_COMM_WORLD, &statusOben);
+		MPI_Recv(&rechtsGhost, 14, MPI_INT, 3, 97, MPI_COMM_WORLD,
+				&statusRechts);
+		MPI_Recv(&untenGhost, 14, MPI_INT, 0, 98, MPI_COMM_WORLD, &statusUnten);
+		MPI_Recv(&linksGhost, 14, MPI_INT, 3, 99, MPI_COMM_WORLD, &statusLinks);
 
 	}
 	if (rank == 3) {
 
-		int xstart, ystart, xende, yende;
+		outputProcessInformation(rank, xstart, ystart, xende, yende, comm_cart);
 
-		xstart = w / 2;
-		xende = w;
-		ystart = h / 2;
-		yende = h;
+		MPI_Recv(&obenGhost, 14, MPI_INT, 1, 96, MPI_COMM_WORLD, &statusOben);
+		MPI_Recv(&rechtsGhost, 14, MPI_INT, 2, 97, MPI_COMM_WORLD,
+				&statusRechts);
+		MPI_Recv(&untenGhost, 14, MPI_INT, 1, 98, MPI_COMM_WORLD, &statusUnten);
+		MPI_Recv(&linksGhost, 14, MPI_INT, 2, 99, MPI_COMM_WORLD, &statusLinks);
 
-
-		printf("--------------------------------------------\n");
-
-		printf("My rank: %d\n", rank);
-
-		printf("Section: von P(%d|%d) bis P(%d|%d) \n\n", xstart,ystart,xende,yende);
-		printf("Position:\n");
-		printf("00\n");
-		printf("0X\n");
-
-
-		int * rankleft;
-		int *rankright;
-
-		MPI_Cart_shift(comm_cart, 1, 1, &rankleft, &rankright);
-
-		printf("Neighbour-ranks: %d (left) - %d (right) \n\n", rankleft,
-				rankright);
-
-		writeVTK2(TimeSteps, currentfield, "gol", xstart, ystart, xende, yende,
-				"_r3_");
+		MPI_Send(&sendenUntenGhost, 14, MPI_INT, 1, 96, MPI_COMM_WORLD);
+		MPI_Send(&sendenRechtsGhost, 14, MPI_INT, 2, 97, MPI_COMM_WORLD);
+		MPI_Send(&sendenObenGhost, 14, MPI_INT, 1, 98, MPI_COMM_WORLD);
+		MPI_Send(&sendenLinksGhost, 14, MPI_INT, 2, 99, MPI_COMM_WORLD);
 
 	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	/*Der erhaltene Ghostrand wird dem prozesseigenen part_field_with_ghost hinzugefügt.
+	 * ------------------------------------------------------------------------------------------------------------------------------------
+	 */
+
+	for (int i = 0; i < (w / 2); i++) {
+		printf("%d %d %d %d %d\n", i, obenGhost[i], rechtsGhost[i],
+				untenGhost[i], linksGhost[i]);
+
+		part_field_with_ghost[calcIndex(w, 0, i + 1)] = linksGhost[i];
+		part_field_with_ghost[calcIndex(w, ((w / 2) + 1), i + 1)] =
+				rechtsGhost[i];
+		part_field_with_ghost[calcIndex(w, i + 1, 0)] = obenGhost[i];
+		part_field_with_ghost[calcIndex(w, i + 1, ((w / 2) + 1))] =
+				untenGhost[i];
+	}
+
+	if (rank == 0) {
+		writeVTK2(TimeSteps, part_field_with_ghost, "gol", xende - xstart,
+				yende - ystart, "_r0_");
+	} else if (rank == 1) {
+		writeVTK2(TimeSteps, part_field_with_ghost, "gol", xende - xstart,
+				yende - ystart, "_r1_");
+	} else if (rank == 2) {
+		writeVTK2(TimeSteps, part_field_with_ghost, "gol", xende - xstart,
+				yende - ystart, "_r2_");
+	} else if (rank == 3) {
+		writeVTK2(TimeSteps, part_field_with_ghost, "gol", xende - xstart,
+				yende - ystart, "_r3_");
+	}
+
+	printf("\n Current Part field with Exchange \n");
+
+//         evolve(current_part_field, new_part_field, w, h, 1, 15, 1, 15);
+//         printf("\n After Evoling\n");
+	exit(1);
+
+	MPI_Barrier(MPI_COMM_WORLD);
 
 	/*
 	 for (t = 0; t < TimeSteps; t++) {
@@ -294,6 +340,38 @@ void game(int w, int h) {
 
 	free(currentfield);
 	free(newfield);
+
+}
+
+void outputProcessInformation(int rank, int xstart, int ystart, int xende,
+		int yende, MPI_Comm comm_cart) {
+
+	printf("--------------------------------------------\n");
+	printf("My rank: %d\n", rank);
+	printf("Section: von P(%d|%d) bis P(%d|%d) \n\n", xstart, ystart, xende,
+			yende);
+
+	printf("Position:\n");
+	if (rank == 0) {
+		printf("X0\n");
+		printf("00\n");
+	} else if (rank == 1) {
+		printf("00\n");
+		printf("X0\n");
+	} else if (rank == 2) {
+		printf("0X\n");
+		printf("00\n");
+	} else if (rank == 3) {
+		printf("00\n");
+		printf("0X\n");
+	}
+
+	int *rankleft;
+	int *rankright;
+
+	MPI_Cart_shift(comm_cart, 1, 1, &rankleft, &rankright);
+
+	printf("Neighbour-ranks: %d (left) - %d (right) \n\n", rankleft, rankright);
 
 }
 
